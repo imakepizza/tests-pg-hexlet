a4f1e6e61dc0c465f314f899feab5b7674797eed
imakepizza <anpash1317@gmail.com>
2022-11-28 10:24:12 +0900

    change throw errors to asssert function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,5 @@
 export const capitalize = (text) => {
+  if(text.length === 0) return '';
   const [firstSymbol = '', ...restSymbols] = text;
   return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
 };
